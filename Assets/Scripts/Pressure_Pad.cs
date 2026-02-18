1267913a8a567e5b0771fbc9fa78f048a8d0225d
Zachary Davey <zdavey@mail.fresnostate.edu>
2026-02-17 19:48:30 -0800

    Completed pressure plate mechanics and added code to pause script that freezes animations too

--- Assets/Scripts/Pressure_Pad.cs
+++ Assets/Scripts/Pressure_Pad.cs
@@ -1,18 +1,22 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Pressure_Pad : MonoBehaviour
 {
-    
+    public UnityEvent objectOn;
+    public UnityEvent objectOff;
     
     private void OnTriggerEnter(Collider other)
     {
         //open door (message?)
         Debug.Log("object on plate");
+        objectOn.Invoke();
     }
 
     private void OnTriggerExit(Collider other)
     {
         //stop opening door (message?)
         Debug.Log("Object removed from plate");
+        objectOff.Invoke();
     }
 }
