3e03011035e3e6d37f13be0b79260456107a0c35
Zachary Davey <zdavey@mail.fresnostate.edu>
2026-03-10 17:34:28 -0700

    Made door controlled by two pressure plates, need to fix animation

--- Assets/Scripts/Pressure_Pad.cs
+++ Assets/Scripts/Pressure_Pad.cs
@@ -5,18 +5,26 @@ public class Pressure_Pad : MonoBehaviour
 {
     public UnityEvent objectOn;
     public UnityEvent objectOff;
+    private int pressCount = 0;
     
     private void OnTriggerEnter(Collider other)
     {
-        //open door (message?)
-        Debug.Log("object on plate");
-        objectOn.Invoke();
+        
+        //Debug.Log("object on plate");
+        pressCount++;
+        if (pressCount == 1)
+        {
+            objectOn.Invoke();
+        }
     }
 
     private void OnTriggerExit(Collider other)
     {
-        //stop opening door (message?)
-        Debug.Log("Object removed from plate");
-        objectOff.Invoke();
+        //Debug.Log("Object removed from plate");
+        pressCount = Mathf.Max(0, pressCount - 1);
+        if (pressCount == 0)
+        {
+            objectOff.Invoke();
+        }
     }
 }
