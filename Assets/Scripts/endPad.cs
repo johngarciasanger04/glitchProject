e2a1e1ea25bc7121637d3a08095fb4c83a5eaaa0
SKL <sebkplor@gmail.com>
2026-04-14 17:12:01 -0700

    End Pad Prefab Created. Modified Script for End Pad to transition through levels as Player Progresses properly from Levels 1 - 3

--- Assets/Scripts/endPad.cs
+++ Assets/Scripts/endPad.cs
@@ -1,7 +1,14 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class endPad : MonoBehaviour
 {
+    string sceneName = "";
+    void Start() {
+        Cursor.visible = false; // Hide the cursor
+        Cursor.lockState = CursorLockMode.Locked; // Lock the cursor to the center of the screen
+        sceneName = SceneManager.GetActiveScene().name;
+    }
     // Script to detect when the player reaches the end pad and trigger the end of the level
     private void OnTriggerEnter(Collider other)
     {
@@ -11,7 +18,20 @@ public class endPad : MonoBehaviour
             Debug.Log("Level Complete!");    
             Cursor.visible = true; // Make the cursor visible    
             Cursor.lockState = CursorLockMode.None; // Unlock the cursor
-            UnityEngine.SceneManagement.SceneManager.LoadScene("MainMenu_Scene");
+
+            //UnityEngine.SceneManagement.SceneManager.LoadScene("MainMenu_Scene");
+                if (sceneName == "Level 1")
+                {
+                    UnityEngine.SceneManagement.SceneManager.LoadScene("Pressure plate level");
+                }
+                else if (sceneName == "Pressure plate level")
+                {
+                    UnityEngine.SceneManagement.SceneManager.LoadScene("Maze Level");
+                }
+                else if (sceneName == "Maze Level")
+                {
+                    UnityEngine.SceneManagement.SceneManager.LoadScene("MainMenu_Scene");
+                }
 
         }
     }
